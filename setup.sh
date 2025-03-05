47b3e5718239531b7f3aca3751f7433ff704ef11
dinedev-24 <dine24@gmail.com>
2025-03-05 11:48:27 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-pip uninstall streamlit -y
-pip install --upgrade pip
+
+# Upgrade pip and install necessary dependencies
+pip install --upgrade pip setuptools wheel
+
+# Install Rust for tokenizers
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
